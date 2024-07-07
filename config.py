fed8df9aae0a5bb25d3fe7556d1b9efb6363520d
MattJeff <mhiguinen235@gmail.com>
2024-07-08 03:21:21 +0700

    Add python-dotenv to requirements and update config

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Charger les variables d'environnement depuis le fichier .env
-
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+class Config:
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
